ccf7accfb2376e52fe7e3a26ef839a792abecd18
Joben Gabin <Barbatosjaegar@gmail.com>
2024-06-25 20:26:51 +0800

    added the forms and relative components

--- frmSignUp1.vb
+++ frmSignUp1.vb
@@ -1,3 +1,13 @@
-﻿Public Class Form1
+﻿Public Class frmSignUp1
+    Private Sub Guna2HtmlLabel1_Click(sender As Object, e As EventArgs) Handles lblPasswordNotice.Click
 
+    End Sub
+
+    Private Sub Guna2HtmlLabel1_Click_1(sender As Object, e As EventArgs) Handles Guna2HtmlLabel1.Click
+
+    End Sub
+
+    Private Sub Guna2HtmlLabel2_Click(sender As Object, e As EventArgs) Handles Guna2HtmlLabel2.Click
+
+    End Sub
 End Class
